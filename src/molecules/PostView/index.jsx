9006bc8157887df0e3a7b8ce1e47ec76a00168ea
montagy <jencean@126.com>
2017-08-11 23:12:28 +0800

    add style of postview

--- src/molecules/PostView/index.jsx
+++ src/molecules/PostView/index.jsx
@@ -5,18 +5,20 @@ import styles from './index.css';
 const PostView = ({ post, comments }) => {
   const cms = comments.map((cm, i) => {
     return (
-      <div key={i}>
+      <div key={i} className={styles.comment}>
         <p>author:{cm.author}</p>
         <p dangerouslySetInnerHTML={{ __html: htmlDecode(cm.body_html)}} />
       </div>
     );
   });
   return (
-    <div>
-      <p>{post.title}</p>
-      <p dangerouslySetInnerHTML={{ __html: htmlDecode(post.selftext_html) }} />
+    <div className={styles.wrapper}>
+      <div className={styles.post}>
+        <p>{post.title}</p>
+        <p dangerouslySetInnerHTML={{ __html: htmlDecode(post.selftext_html) }} />
+      </div>
       {comments.length
-        ? <div>
+        ? <div className={styles.content}>
             <p>Comment</p>
             {cms}
           </div>
