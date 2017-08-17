64eea13f6b7dbb67f7115b107aeeefdaaa41204f
montagy <jencean@126.com>
2017-08-17 14:54:29 +0800

    use section instead of div

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Home = () =>
-  <div>
+  <section>
     <Link to="/r/haskell">Haskell</Link>
     <Link to="/r/reactjs">ReactJs</Link>
-  </div>;
+  </section>;
 
 export default Home;
