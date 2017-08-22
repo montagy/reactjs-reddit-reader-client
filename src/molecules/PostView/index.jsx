b55c7ebf97c8673e7948ceeb24847759c9e2ca06
montagy <jencean@126.com>
2017-08-22 15:55:34 +0800

    外观调整

--- src/molecules/PostView/index.jsx
+++ src/molecules/PostView/index.jsx
@@ -2,20 +2,24 @@ import React from 'react';
 import { htmlDecode } from '../../utils';
 import styles from './index.css';
 
+const Comment = ({ comment }) =>
+  <div className={styles.comment}>
+    <p>author:{comment.author}</p>
+    <div
+      dangerouslySetInnerHTML={{ __html: htmlDecode(comment.body_html) }} />
+  </div>;
+
 const PostView = ({ post, comments }) => {
   const cms = comments.map((cm, i) => {
-    return (
-      <div key={i} className={styles.comment}>
-        <p>author:{cm.author}</p>
-        <p dangerouslySetInnerHTML={{ __html: htmlDecode(cm.body_html)}} />
-      </div>
-    );
+    return <Comment key={i} comment={cm} />;
   });
   return (
     <div className={styles.wrapper}>
       <div className={styles.post}>
-        <p>{post.title}</p>
-        <p dangerouslySetInnerHTML={{ __html: htmlDecode(post.selftext_html) }} />
+        <header>{post.title}<span>{post.author}</span></header>
+        <main className="markdown-body"
+          dangerouslySetInnerHTML={{ __html: htmlDecode(post.selftext_html) }}
+        />
       </div>
       {comments.length
         ? <div className={styles.content}>
