90c823f9b7286c676d4560a1caa4d435d7c7ac1c
montagy <jencean@126.com>
2017-08-23 12:18:16 +0800

    adjust ToggleButton

--- src/molecules/ToggleButton/index.jsx
+++ src/molecules/ToggleButton/index.jsx
@@ -2,17 +2,18 @@ import React from 'react';
 import styles from './index.css';
 import classNames from 'classnames';
 
+//TODO use svg icon instead
 const ToggleButton = ({ active, handleToggle }) => {
   const cls = classNames({
     [styles.toggle]: true,
     [styles.active]: active,
   });
   return (
-    <button onClick={handleToggle} className={cls}>
-      <span className={active ? styles.firstBar : null} />
-      <span className={active ? styles.sndBar : null} />
-      <span className={active ? styles.lastBar : null} />
-    </button>
+    <div onClick={handleToggle} className={cls}>
+      <div className={active ? styles.firstBar : null} />
+      <div className={active ? styles.sndBar : null} />
+      <div className={active ? styles.lastBar : null} />
+    </div>
   );
 };
 
