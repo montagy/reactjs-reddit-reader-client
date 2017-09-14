fe34807a6c4db1ed83d462aaf80283058ea944be
montagy <jencean@gmail.com>
2017-09-14 19:48:56 +0800

    clean

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -5,68 +5,58 @@ import styles from './index.css';
 import InlineForm from '../../molecules/InlineForm';
 import '!style-loader!css-loader!react-input-range/lib/css/index.css';
 
+Home.propTypes = {
+  defaultHome: string,
+  setDefaultHome: func,
+  cachedHour: number,
+  setFontSize: func.isRequired,
+};
 //TODO defaultHome 的有效性检测
-class Home extends React.Component {
-  state = {
-    value: 1,
-  };
-  handleChange = value => {
-    this.setState({ value });
-    this.props.setFontSize;
-  };
-  static propTypes = {
-    defaultHome: string,
-    setDefaultHome: func,
-    cachedHour: number,
-    setFontSize: func.isRequired,
-  };
-  render() {
-    const {
-      defaultHome,
-      cachedHour,
-      setDefaultHome,
-      fontSize,
-      setFontSize,
-    } = this.props;
-    return (
-      <div className={styles.wrapper}>
-        <header>
-          <h1>设置</h1>
-        </header>
-        <div className={styles.container}>
+function Home({
+  defaultHome,
+  cachedHour,
+  setDefaultHome,
+  fontSize,
+  setFontSize,
+}) {
+  return (
+    <div className={styles.wrapper}>
+      <header>
+        <h1>设置</h1>
+      </header>
+      <div className={styles.container}>
+        <div>
+          <p>首页: {defaultHome}</p>
+          <InlineForm onSubmit={setDefaultHome} />
+        </div>
+        <div>
+          <p>缓存{cachedHour}小时</p>
           <div>
-            <p>首页: {defaultHome}</p>
-            <InlineForm onSubmit={setDefaultHome} />
+            <form
+              onSubmit={e => {
+                e.preventDefault();
+              }}
+              className={styles.lineForm}
+            >
+              <input type="text" />
+              <button type="submit">提交</button>
+            </form>
           </div>
-          <div>
-            <p>缓存{cachedHour}小时</p>
-            <div>
-              <form
-                onSubmit={e => {
-                  e.preventDefault();
-                }}
-                className={styles.lineForm}
-              >
-                <input type="text" />
-                <button type="submit">提交</button>
-              </form>
-            </div>
-          </div>
-          <div>
-            <p>字体大小</p>
-            <div style={{ width: '50%' }}>
-              <InputRange
-                maxValue={5}
-                minValue={1}
-                value={fontSize}
-                onChange={setFontSize}
-              />
-            </div>
+        </div>
+        <div>
+          <p>字体大小</p>
+          <div style={{ width: '50%' }}>
+            <InputRange
+              maxValue={5}
+              minValue={1}
+              value={fontSize}
+              onChange={setFontSize}
+            />
           </div>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default Home;
