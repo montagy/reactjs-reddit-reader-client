583cdd230e84f57ebc3abdf2dd25feb9a2889ca2
montagy <jencean@gmail.com>
2017-09-19 18:03:37 +0800

    config

--- src/index.jsx
+++ src/index.jsx
@@ -3,11 +3,21 @@ import ReactDOM from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
 import App from './containers/AppContainer';
 import './index.css';
+import config from './mobx/config';
+import { Provider } from 'mobx-react';
+import { useStrict } from 'mobx';
 
+useStrict(true);
+
+const store = {
+  config,
+};
 const render = Comp => {
   ReactDOM.render(
     <AppContainer>
-      <Comp />
+      <Provider {...store}>
+        <Comp />
+      </Provider>
     </AppContainer>,
     document.getElementById('root'),
   );
--- src/molecules/index.js
+++ src/molecules/index.js
@@ -2,3 +2,5 @@ export ToggleButton from './ToggleButton';
 export Pannel from './Pannel';
 export PostView from './PostView';
 export Summary from './Summary';
+export Affix from './Affix';
+export InlineForm from './InlineForm';
